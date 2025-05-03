8bf6738eb5c6aa6ffa247f9e4727870b42f050b3
Trisha <akumar@Trishas-MacBook-Air.local>
2025-05-03 12:34:17 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';  // Import the CSS file
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/library-management"r>
       <div>
         {/* Navigation Bar */}
         <nav className="navbar">
